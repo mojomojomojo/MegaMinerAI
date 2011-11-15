d885a0817e0eec488b49ba96af2030f4a554c3c8
thartman <thartman@rc05xcs213.managed.mst.edu>
2011-11-15 08:27:52 -0600

    removed unecessary extensions in base name

---
+++ BotNet/client/CrazyUncleBurton/c/chains/AttackChain.cpp
@@ -0,0 +1,18 @@
+#include "AttackChainH.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+AttackChain::AttackChain(AI* ai) : m_ai(ai)
+{
+}
+
+bool AttackChain::spawnMore()
+{
+}
+
+bool AttackChain::advance()
+{
+}
+
+bool AttackChain::kidnap(int virusID)
+{
+}
---
+++ BotNet/client/CrazyUncleBurton/c/chains/ExploreChain.cpp
@@ -0,0 +1,18 @@
+#include "ExploreChainH.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+ExploreChain::ExploreChain(AI* ai) : m_ai(ai)
+{
+}
+
+bool ExploreChain::spawnMore()
+{
+}
+
+bool ExploreChain::advance()
+{
+}
+
+bool ExploreChain::kidnap(int virusID)
+{
+}
